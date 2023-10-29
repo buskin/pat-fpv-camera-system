dbd970c3ed6ed663277284795851a27bed1437e5
Mikhail Buskin <mikhail.buskin@gmail.com>
2023-10-29 12:01:18 -0700

    .

--- IMU_Tilt_Roll_Pan/IMU_Tilt_Roll_Pan.ino
+++ IMU_Tilt_Roll_Pan/IMU_Tilt_Roll_Pan.ino
@@ -6,12 +6,11 @@ float gx_cal, gy_cal, gz_cal;
 
 // Variables for time and angle calculation
 unsigned long previousTime = 0;
-float elapsedTime, dt;
+
 float angleX = 0.0;  // Initial angle
 float angleY = 0.0;
 float angleZ = 0.0;
 
-long loop_timer;
 long i = 0;
 float angleCoefficient = 180.0 / 155.0;
 
@@ -45,8 +44,8 @@ void loop() {
   IMU.readGyroscope(gx, gy, gz);
 
   // Calculate time since the last loop execution
-  unsigned long currentTime = millis();
-  elapsedTime = (currentTime - previousTime) / 1000.0;  // Convert to seconds
+  unsigned long currentTime = micros();
+  float elapsedTime = (currentTime - previousTime) / 1000000.0;  // Convert to seconds
   previousTime = currentTime;
 
   gx -= gx_cal;
@@ -69,11 +68,10 @@ void loop() {
   }
 
   i++;
-  // delay(1);
-  wewait(1000);
+  //delay(0);
+  wait(100);
 }
 
-void wewait(long time) {
-  while (micros() - loop_timer < time)
-  loop_timer = micros();  //Reset the loop timer
+void wait(long time) {
+  while (micros() - previousTime < time);
 }
